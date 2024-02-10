0f9d8118d5575da1df50455226ecb8898764f741
Andres Flores <andresflo883@gmail.com>
2024-02-10 14:57:08 -0500

    added jobs view

--- BulkyBookWeb/Controllers/HomeController.cs
+++ BulkyBookWeb/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Jobs()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
